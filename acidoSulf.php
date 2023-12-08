fec84bdaee7a4a0d438bb76e4aec6de7f38aa49f
Benjamin Poblete Meza <124107286+benjapob@users.noreply.github.com>
2023-12-08 12:24:16 -0300

    firebase

--- acidoSulf.php
+++ acidoSulf.php
@@ -5,10 +5,10 @@ $url = 'https://senmin-cb7cc-default-rtdb.firebaseio.com/';
 // --- Consume el Token de la base de datos en la siguiente linea
 $token = '7HElDOyIX5VNIGrlCwBthEkHD4W2XD16VP2EUXtm';
 // --- Desde aquí obtiene el parametro que se enviará por http GET
-$arduino_data = $_GET['gasValor'];
+$arduino_data = $_GET['acidoSulfValor'];
 // --- $arduino_data_post = $_POST['name'];
 // --- Aquí asigna la parte de la estrucutra de la base de datos donde irá el parámetro
-$firebasePath = '/Dispositivo/0/gas';
+$firebasePath = '/Dispositivo/0/acidoSulf';
 /// --- Generar llamadas
 $fb = new fireBase($url, $token);
 $response = $fb->set($firebasePath, $arduino_data);
